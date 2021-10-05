8caf05607225123d37ee4835f5e039e0386c5c4b
kentakki416 <fujiken0814@outlook.jp>
2021-10-05 12:57:35 +0900

    Herokuの環境設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
-        Schema::defaultStringLength(250);
+        Schema::defaultStringLength(191);
     }
 }
